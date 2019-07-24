1a571ff5cbf28c7e98090aa8561feb0dd66c7050
Maria Alejandra Coy Ulloa <malejacoy@hotmail.com>
2019-07-24 19:54:10 +0000

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,6 +8,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t n_leaves = 0;
 
+
 	if (!tree)
 		return (0);
 	if (tree->parent != NULL && tree->left == NULL && tree->right == NULL)
