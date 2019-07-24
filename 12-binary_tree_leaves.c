aaf0db1b1b2b425af32499a4021e909a417e8b69
carlosariasf <carlosarias@icreativa.com.co>
2019-07-24 20:13:48 +0000

    Fix 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,7 +11,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->parent != NULL && tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1 + (binary_tree_leaves(tree->left)
 			+ binary_tree_leaves(tree->right)));
 	n_leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
