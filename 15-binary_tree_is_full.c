86ec23f215d6e78dfa0e47233462e3b232052eda
carlosariasf <carlosarias@icreativa.com.co>
2019-07-24 22:58:31 +0000

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,13 +7,11 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (0);
+		return (1);
 	if (tree->left == NULL && tree->right == NULL)
-		binary_tree_is_full(tree->left);
-		binary_tree_is_full(tree->right);
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
-	return (1);
+		return (1);
+	if ((tree->left) && (tree->right))
+	return(binary_tree_is_full(tree->left) &&
+	binary_tree_is_full(tree->right));
+	return (0);
 }
